8cae24292a54266b41e20f618e8357d7f6a6a072
Esteban SP <esilvapellet@gmail.com>
2024-05-15 23:21:51 -0400

    carga inicial desafío

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "/m6-d3-binding-formularios/"
 })
